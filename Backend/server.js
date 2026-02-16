c1d9db43e3b991da23bf4744a990b62978607e24
AnujDalvadi <anujdalwadi33@gmail.com>
2026-02-16 22:53:35 +0530

    server handele vercel deploy

--- Backend/server.js
+++ Backend/server.js
@@ -2,8 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import database_connect from "./database/db.js";
-import { fileURLToPath } from "url";
-import path from "path";
 import UserRoutes from "./Routes/UserRoutes.js";
 
 dotenv.config();
@@ -15,13 +13,9 @@ const app = express();
 //middleware origin websites
 app.use(cors());
 const corsOption = {
-  origin: "http://localhost:5173",
+  origin: "https://facebook-clone-psi-nine.vercel.app/",
   optionsSuccessStatus: true,
 };
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(cors(corsOption));
 app.use(express.json());
