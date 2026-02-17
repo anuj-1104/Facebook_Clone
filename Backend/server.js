955b6ff850b7fe426b02400d099780652d28d18b
Anuj Dalvadi <anujdalwadi33@gmail.com>
2026-02-17 20:55:43 +0530

    Remove server listen log from server.js

--- Backend/server.js
+++ Backend/server.js
@@ -34,7 +34,3 @@ app.get("/", (_, res) => {
 
 //all Routes others
 app.use("/api/user", UserRoutes);
-
-app.listen(port, () => {
-  console.log(`server is running on - http://localhost:${port}`);
-});
