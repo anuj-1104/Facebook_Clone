da31ae71856e7a66b3e6306e775585173a1d5ea2
AnujDalvadi <anujdalwadi33@gmail.com>
2026-02-17 21:39:46 +0530

    "server erro not database connected atlas"

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
+//check the url find or not
 const database_connect = async () => {
-  try {
-    await mongoose.connect(process.env.URL_MONGODB);
-    console.log("Database Connected");
-  } catch (error) {
-    console.log(error);
+  console.log("MONGO URI:", process.env.URL_MONGODB);
+
+  if (!process.env.URL_MONGODB) {
+    throw new Error("MONGO_URI missing");
   }
+
+  return await mongoose.connect(process.env.URL_MONGODB);
 };
 
 export default database_connect;
